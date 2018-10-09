0bdbb67edc0c3d22491174af697baf19d7cc5bf8
Waqqas Sheikh <wsheikh@tribalscale.com>
2018-10-09 10:50:12 +0400

    handle actions

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,9 @@
 export default (oldState = {value: 0}, action) => {
 	switch(action.type){
+		case "INCREMENT":
+			return {value: value+1};
+		case "DECREMENT":
+			return {value: value-1};
 		default:
 			return oldState;
 	}
