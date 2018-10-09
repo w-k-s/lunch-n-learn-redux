11ffd3f09065bca547da2ad7811246bd54460ad5
Waqqas Sheikh <wsheikh@tribalscale.com>
2018-10-09 10:52:32 +0400

    empty out function bodies of increment() and decrement()

--- src/Counter.js
+++ src/Counter.js
@@ -4,15 +4,11 @@ class Counter extends React.Component {
   state = { count: 0 }
 
   increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
+    
   }
 
   decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
+    
   }
 
   render() {
