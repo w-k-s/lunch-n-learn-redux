f86d18e61dcf5ecde583fee1195133fcdaed686d
Waqqas Sheikh <wsheikh@tribalscale.com>
2018-10-09 11:24:06 +0400

    connect state to component

--- src/Counter.js
+++ src/Counter.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class Counter extends React.Component {
-  state = { count: 0 }
 
   increment = () => {
-    
+    this.props.increment();
   }
 
   decrement = () => {
-    
+    this.props.decrement();
   }
 
   render() {
+    let {value} = this.props;
+
     return (
       <div>
         <h2>Counter</h2>
         <div>
           <button onClick={this.decrement}>-</button>
-          <span>{this.state.count}</span>
+          <span>{value}</span>
           <button onClick={this.increment}>+</button>
         </div>
       </div>
@@ -25,4 +27,21 @@ class Counter extends React.Component {
   }
 }
 
-export default Counter;
+const mapStateToProps = (state)=>{
+  return {
+    value: state.value
+  }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+  return {
+    increment:()=>{
+      dispatch({type: "INCREMENT"})
+    },
+    decrement:()=>{
+      dispatch({type: "DECREMENT"})
+    }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Counter);
